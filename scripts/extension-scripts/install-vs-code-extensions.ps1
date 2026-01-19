e1277f9c0cd3d3fb156843959f69c5f10fd5ed27
Mark J Price <markjprice@gmail.com>
2026-01-19 15:14:39 +0000

    Add code solutions for Ch02

---
+++ scripts/extension-scripts/install-vs-code-extensions.ps1
@@ -0,0 +1,8 @@
+code --install-extension ms-vscode.live-server
+code --install-extension yzhang.markdown-all-in-one
+code --install-extension alexcvzz.vscode-sqlite
+code --install-extension ms-python.python
+code --install-extension batisteo.vscode-django
+code --install-extension humao.rest-client
+code --install-extension github.copilot
+code --install-extension google.geminicodeassist
---
+++ scripts/extension-scripts/uninstall-vs-code-extensions.ps1
@@ -0,0 +1,8 @@
+code --uninstall-extension ms-vscode.live-server
+code --uninstall-extension yzhang.markdown-all-in-one
+code --uninstall-extension alexcvzz.vscode-sqlite
+code --uninstall-extension ms-python.python
+code --uninstall-extension batisteo.vscode-django
+code --uninstall-extension humao.rest-client
+code --uninstall-extension github.copilot
+code --uninstall-extension google.geminicodeassist
